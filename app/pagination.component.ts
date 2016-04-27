3aaf8ab2f287c70db9e51edfe794c489d8e6faca
Justin Joseph <justin.joseph0@yahoo.com>
2016-04-27 18:58:36 -0400

    Progress for pagination implementation

--- app/pagination.component.ts
+++ app/pagination.component.ts
@@ -1,15 +1,39 @@
 import {Component, Input, Output, EventEmitter, OnChanges} from 'angular2/core';
 
 @Component({
-
+	selector: 'pagination',
+	template: `
+		<nav *ngIf="items.length > pageSize">
+		  <ul class="pagination">
+		    <li>
+		      <a href="#" aria-label="Previous">
+		        <span aria-hidden="true">&laquo;</span>
+		      </a>
+		    </li>
+		    <li *ngFor="#page of pages"><a>{{ page }}</a></li>
+		    <li>
+		      <a href="#" aria-label="Next">
+		        <span aria-hidden="true">&raquo;</span>
+		      </a>
+		    </li>
+		  </ul>
+		</nav>
+	`
 })
-export class PaginationComponent {
-	@Input() items;
-	@Input('page-size') pageSize: number = 10;
+export class PaginationComponent implements OnChanges {
+	@Input() items = [];
+	pageSize = 10;
 	pages: any[];
 	currentPage;
 
 	ngOnChanges() {
+		this.currentPage = 1;
 
+		var pagesCount = this.items.length / this.pageSize;
+		this.pages = [];
+
+		for ( var i = 1; i == pagesCount; i++ ) {
+			this.pages.push(i);
+		}
 	}
 }
